50f9ce66ead657ae1dd900d21edb018d7c5472b5
mtillman24 <mayatillman24@gmail.com>
2024-01-12 08:53:22 -0500

    push

--- CreativeCoding/P5/FinalBG/sketch.js
+++ CreativeCoding/P5/FinalBG/sketch.js
@@ -1,7 +1,33 @@
+//adding a GIF to my grid
+
+let gif;
+
+function preload (){
+  gif = loadImage ('assets/MayaGIF.gif');
+}
+
 function setup() {
- 
+  //runs one time
+ createCanvas (windowWidth, windowHeight);
+
 }
 
 function draw() {
+  //runs in a loop 
+  background ('pink');
 
+  var num = 5;
+  var sideLen = windowWidth/num;
+  
+  for(var y = 0; y < windowHeight; y = y + sideLen) {
+    //loop to create a grid in the y direction
+    
+  for (var x = 0; x < windowWidth; x = x + sideLen) {
+  image (gif, x, y, sideLen, SideLen);
+  }
+  }
+  }
+
+function windowResized(){
+  resizeCanvas ( windiwWidth, windowHeight);
 }
