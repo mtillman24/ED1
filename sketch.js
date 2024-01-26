a5575b244a082b0e69e1a29de9081a72d9401bf1
mtillman24 <mayatillman24@gmail.com>
2024-01-26 12:59:39 -0500

    push

--- sketch.js
+++ sketch.js
@@ -19,10 +19,10 @@ function draw() {
   var num = 5;
   var sideLen = windowWidth/num;
   
-  for(var y = 0; y < windowHeight; y = y + sideLen) {
+  for(var y = 0; y < 2 * windowHeight; y = y + sideLen) {
     //loop to create a grid in the y direction
     
-  for (var x = 0; x < windowWidth; x = x + sideLen) {
+  for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
   image (gif, x, y, sideLen, sideLen);
   }
   }
